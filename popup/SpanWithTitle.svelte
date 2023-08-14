c3e013144fa0976a07e24c53e023f2021153e748
iron4umx <140280184+iron4umx@users.noreply.github.com>
2023-08-14 00:47:10 -0400

    Update SpanWithTitle.svelte
    Added detailed comments.

--- popup/SpanWithTitle.svelte
+++ popup/SpanWithTitle.svelte
@@ -1,8 +1,13 @@
 <script>
-	let span;
-	$: title = span && span.textContent;
+  // Declare a variable to hold a reference to the <span> element
+  let span;
+
+  // Computed property that updates the 'title' based on the span's text content
+  $: title = span && span.textContent;
 </script>
 
+<!-- Create a <span> element and bind it to the 'span' variable using 'bind:this' -->
 <span bind:this={span} title={title}>
-	<slot></slot>
+  <!-- Render the content within the <slot> element -->
+  <slot></slot>
 </span>
