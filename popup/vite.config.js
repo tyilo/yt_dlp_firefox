20d3c9b9fb0ebc3a70b1e1f38216df6b47612681
iron4umx <140280184+iron4umx@users.noreply.github.com>
2023-08-14 01:15:37 -0400

    Update vite.config.js
    Added detailed comments.

--- popup/vite.config.js
+++ popup/vite.config.js
@@ -1,12 +1,12 @@
-import svelte from "rollup-plugin-svelte";
-import sveltePreprocess from "svelte-preprocess";
+import svelte from "rollup-plugin-svelte";        // Import the Svelte Rollup plugin
+import sveltePreprocess from "svelte-preprocess"; // Import the Svelte preprocessor
 
 export default {
-  base: '',
+  base: '',                                      // The base path for your application (empty in this case)
   plugins: [
     svelte({
-      preprocess: sveltePreprocess(),
-      emitCss: false,
+      preprocess: sveltePreprocess(),             // Apply Svelte preprocessor to your components
+      emitCss: false,                             // Do not emit CSS as separate files (it will be included in JS)
     }),
   ],
 };
