80241f40b41e87ec5f2771fe2bf99151ddea380e
iron4umx <140280184+iron4umx@users.noreply.github.com>
2023-08-14 17:49:52 -0400

    Update yt_dlp_firefox.bat
    Detailed explanation for a short one liner that is crucial, just like a little bottle of perfume ;-)

--- windows_helper_installer/yt_dlp_firefox.bat
+++ windows_helper_installer/yt_dlp_firefox.bat
@@ -1,2 +1,18 @@
 @echo off
+REM Batch Script to Run yt_dlp_firefox.py
+REM ------------------------------------
+
+REM This batch script is designed to facilitate the execution of the yt_dlp_firefox.py Python script.
+REM It suppresses command echoing to provide a cleaner output during execution.
+
+REM Usage Instructions:
+REM -------------------
+REM 1. Save this batch script in the same directory as yt_dlp_firefox.py.
+REM 2. Open a command prompt in the script's directory.
+REM 3. Run this batch script by typing its name and pressing Enter.
+REM
+REM Note: Ensure that Python is installed and accessible from the command prompt.
+
+REM Execute the Python script using the Python interpreter.
 python yt_dlp_firefox.py
+REM Replace 'yt_dlp_firefox.py' with the actual name of the Python script you want to run.
